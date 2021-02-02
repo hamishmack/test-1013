be10c4346205379f39c2680b1e4ce6bb3e4ef601
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-02-03 00:50:13 +1300

    Put project in default.nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,2 @@
-let
-  sources = import nix/sources.nix {};
-  haskellNix = import sources."haskell.nix" {};
-  pkgs = import haskellNix.sources.nixpkgs haskellNix.nixpkgsArgs;
-  project = pkgs.haskell-nix.project {
-    compiler-nix-name = "ghc8103";
-    # src = ./.;
-    src = pkgs.haskell-nix.haskellLib.cleanGit { name = "test-1013"; src = ./.; };
-  };
+let project = import ./.;
 in project.shellFor { packages = ps: [ ps.bar ]; }
