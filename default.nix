f003490548313d26f3d7cc7de763abf3ed72f4a2
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-02-03 17:50:53 +1300

    Yep it is something to do with stack

--- default.nix
+++ default.nix
@@ -2,8 +2,8 @@ let
   sources = import nix/sources.nix {};
   haskellNix = import sources."haskell.nix" {};
   pkgs = import haskellNix.sources.nixpkgs haskellNix.nixpkgsArgs;
-in pkgs.haskell-nix.project {
-    compiler-nix-name = "ghc8103";
+in pkgs.haskell-nix.stackProject {
+    # compiler-nix-name = "ghc8103";
     # src = ./.;
     src = pkgs.haskell-nix.haskellLib.cleanGit { name = "test-1013"; src = ./.; };
   }
