4a24c75ebf21b6ec7eec851e1fc9c99722d85948
qz <qz@pandaren.local>
2021-02-03 07:02:05 +0300

    1013 reproduces now

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
   sources = import nix/sources.nix {};
   haskellNix = import sources."haskell.nix" {};
-  pkgs = import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
+  pkgs = import haskellNix.sources.nixpkgs haskellNix.nixpkgsArgs;
 in pkgs.haskell-nix.project {
     compiler-nix-name = "ghc8103";
     # src = ./.;
