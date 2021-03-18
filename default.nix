a1963e281f16c1c8c54dcf6dd19b7cead84a6818
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-03-18 15:48:01 +1300

    For testing hkm/stack-clean-fix branch
    Issue https://github.com/input-output-hk/haskell.nix/issues/1013
    Fix https://github.com/input-output-hk/haskell.nix/pull/1074

--- default.nix
+++ default.nix
@@ -2,8 +2,8 @@ let
   sources = import nix/sources.nix {};
   haskellNix = import sources."haskell.nix" {};
   pkgs = import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
-in pkgs.haskell-nix.cabalProject {
-    compiler-nix-name = "ghc8103";
+in pkgs.haskell-nix.stackProject {
+    # compiler-nix-name = "ghc8104";
     # src = ./.;
     src = pkgs.haskell-nix.haskellLib.cleanGit { name = "test-1013"; src = ./.; };
-  }
+  } // { inherit pkgs; }
